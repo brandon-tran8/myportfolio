178936cedc218b7ce337ffefbfe5fc171d068dce
Brandon Tran <brandonwtran@gmail.com>
2024-04-23 21:56:16 -0600

    added bio

--- app/sections/contact.js
+++ app/sections/contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <div
       id={contact.title}
-      className="px-8 sm:px-12 md:px-32 pb-32 flex justify-center align-center flex-col bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"
+      className="px-8 sm:px-12 md:px-32 pb-32 flex justify-center align-center flex-col bg-gradient-to-r from-stone-500 via-rose-400 to-indigo-400"
     >
       <h2 className="mt-12 uppercase font-bold text-center text-white text-bold text-4xl">
         {contact.title}
